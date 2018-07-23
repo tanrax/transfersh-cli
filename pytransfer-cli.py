290f9c5834dcb25881ddce40f40e1c41c5222380
Andros Fenollosa <andros@fenollosa.email>
2018-07-23 18:28:47 +0200

    First commit

---
+++ pytransfer-cli.py
@@ -0,0 +1,27 @@
+import os
+import requests
+import click
+import pyperclip
+
+URL_TRANSFERSH = 'https://transfer.sh'
+
+@click.command()
+@click.argument('filename')
+def transfersh_cli(filename):
+    """ Program that uploads a file to Transfer.sh """
+    try:
+        # Upload file
+        click.echo('Uploading file')
+        conf_file = {'upload_file': filename}
+        r = requests.post(URL_TRANSFERSH, files=conf_file)
+        # Shows route to download
+        download_url = r.text
+        click.echo(f'Download from here: {download_url}')
+        click.echo(f'It has also been copied to the clipboard!')
+        # Copy route to clipboard
+        pyperclip.copy(download_url)
+    except Exception:
+        click.echo('Something has failed. The file could not be uploaded.')
+
+if __name__ == '__main__':
+    transfersh_cli()
