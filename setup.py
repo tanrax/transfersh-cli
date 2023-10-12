41e317421acc8e338b3de19c0d7da68f206908de
Andros Fenollosa <andros@fenollosa.email>
2023-10-12 16:12:59 +0200

    Add requirements and upload version

--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
 from setuptools import setup
+
 setup(
-  name = 'transfersh-cli',
-  py_modules=['transfersh'],
-  version = '1.1.0',
-  python_requires='>3.6',
-  description = 'Client to upload files to the transfer.sh service',
-  author = 'Andros Fenollosa',
-  author_email = 'andros@fenollosa.email',
-  url = 'https://github.com/tanrax/transfersh-cli',
-  keywords = ['transfer', 'upload', 'client'],
-  classifiers=(
+    name="transfersh-cli",
+    py_modules=["transfersh"],
+    version="1.2.0",
+    python_requires=">3.6",
+    description="Client to upload files to the transfer.sh service",
+    author="Andros Fenollosa",
+    author_email="andros@fenollosa.email",
+    url="https://github.com/tanrax/transfersh-cli",
+    keywords=["transfer", "upload", "client"],
+    classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-  ),
-  install_requires=[
-        'Click>=6.7',
-        'pyperclip>=1.6.4',
-        'requests>=2.19.1'
-  ],
-  entry_points='''
+    ),
+    install_requires=[
+        "click==8.1.7",
+        "progressbar2==4.2.0",
+        "pyperclip==",
+        "requests==2.31.0",
+        "requests-toolbelt==",
+        "tqdm==4.66.1",
+    ],
+    entry_points="""
       [console_scripts]
       transfersh=transfersh:transfersh_cli
-  '''
+  """,
 )
--- transfersh.py
+++ transfersh.py
@@ -11,49 +11,57 @@ from tqdm import tqdm
 from requests_toolbelt import MultipartEncoder, MultipartEncoderMonitor
 
 # Variables
-URL_TRANSFERSH = 'https://transfer.sh'
+URL_TRANSFERSH = "https://transfer.sh"
 
 
 @click.command()
-@click.argument('filename')
-@click.option('-a', '--max-days', type=int, help='Maximum number of days to keep file')
-@click.option('-d', '--max-downloads', type=int, help='Maximum number of times that file can be downloaded')
+@click.argument("filename")
+@click.option("-a", "--max-days", type=int, help="Maximum number of days to keep file")
+@click.option(
+    "-d",
+    "--max-downloads",
+    type=int,
+    help="Maximum number of times that file can be downloaded",
+)
 def transfersh_cli(filename, max_days, max_downloads):
-    """ Program that uploads a file to Transfer.sh """
+    """Program that uploads a file to Transfer.sh"""
     download_url = ""
     try:
         # Open file
-        with tqdm(desc=filename,
-                  total=os.path.getsize(filename),
-                  unit='B',
-                  unit_scale=True,
-                  unit_divisor=1_024, ) as bar:
-            with open(filename, 'rb') as data:
+        with tqdm(
+            desc=filename,
+            total=os.path.getsize(filename),
+            unit="B",
+            unit_scale=True,
+            unit_divisor=1_024,
+        ) as bar:
+            with open(filename, "rb") as data:
                 # Upload file
-                fields = {'file': (filename, data)}
+                fields = {"file": (filename, data)}
                 headers = {}
                 # Option to indicate the maximum number of days
                 if max_days is not None:
-                    headers['Max-Days'] = str(max_days)
+                    headers["Max-Days"] = str(max_days)
                 # Option to indicate the maximum number of downloads
                 if max_downloads is not None:
-                    headers['Max-Downloads'] = str(max_downloads)
+                    headers["Max-Downloads"] = str(max_downloads)
 
                 m = MultipartEncoderMonitor(
-                    MultipartEncoder(fields=fields), lambda monitor: bar.update(monitor.bytes_read - bar.n)
+                    MultipartEncoder(fields=fields),
+                    lambda monitor: bar.update(monitor.bytes_read - bar.n),
                 )
-                headers['Content-Type'] = m.content_type
+                headers["Content-Type"] = m.content_type
                 r = requests.post(URL_TRANSFERSH, data=m, headers=headers)
                 download_url = r.text
     except Exception:
-        click.echo('Something has failed. The file could not be uploaded.')
+        click.echo("Something has failed. The file could not be uploaded.")
 
     # Shows route to download
-    click.echo(f'Download from here: {download_url}')
-    click.echo(f'It has also been copied to the clipboard!')
+    click.echo(f"Download from here: {download_url}")
+    click.echo(f"It has also been copied to the clipboard!")
     # Copy route to clipboard
     pyperclip.copy(download_url)
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     transfersh_cli()
